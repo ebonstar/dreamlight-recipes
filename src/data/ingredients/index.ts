57410305fbf0056b0e564f0e01d871887e5f2072
ebonstar <18689574+ebonstar@users.noreply.github.com>
2022-09-28 15:39:06 +0000

    export data for all ingredients

--- src/data/ingredients/index.ts
+++ src/data/ingredients/index.ts
@@ -1,10 +1,21 @@
-export * from "./ingredients";
+import { VEGETABLES_DATA } from "./vegetables";
+import { FRUIT_DATA } from "./fruit";
+import { GRAINS_DATA } from "./grains";
+import { DAIRY_OIL_DATA } from "./dairyoil";
+import { FISH_DATA } from "./fish";
+import { SEAFOOD_DATA } from "./seafood";
+import { SPICES_HERBS_DATA } from "./spicesherbs";
+import { SWEETS_DATA } from "./sweets";
 
-export { VEGETABLES_DATA } from "./vegetables";
-export { FRUIT_DATA } from "./fruit";
-export { GRAINS_DATA } from "./grains";
-export { DAIRY_OIL_DATA } from "./dairyoil";
-export { FISH_DATA } from "./fish";
-export { SEAFOOD_DATA } from "./seafood";
-export { SPICES_HERBS_DATA } from "./spicesherbs";
-export { SWEETS_DATA } from "./sweets";
+export type { Ingredient, IngredientData } from "./ingredients";
+
+export const ALL_INGREDIENT_DATA = {
+  ...VEGETABLES_DATA,
+  ...FRUIT_DATA,
+  ...GRAINS_DATA,
+  ...DAIRY_OIL_DATA,
+  ...FISH_DATA,
+  ...SEAFOOD_DATA,
+  ...SPICES_HERBS_DATA,
+  ...SWEETS_DATA,
+};
